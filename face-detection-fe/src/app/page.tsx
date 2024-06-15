b8f8b39c0f56adca2bd098a192bc0908aa98de76
Parth Sinha <sinhaparth555@gmail.com>
2024-06-15 19:46:37 +0100

    change ui design

--- face-detection-fe/src/app/page.tsx
+++ face-detection-fe/src/app/page.tsx
@@ -7,6 +7,7 @@ import { Button } from "@/components/ui/button";
 import Particles from "@/components/magicui/particles";
 import { useTheme } from "next-themes";
 import GradualSpacing from "@/components/magicui/text-space";
+import { Badge } from "@/components/ui/badge"
 
 export default function Home() {
   const { theme } = useTheme();
@@ -53,17 +54,25 @@ export default function Home() {
       <div style={{ textAlign: 'center', marginTop: '50px' }}>
         <GradualSpacing className="font-display text-center text-4xl font-bold tracking-[-0.1em]  text-black dark:text-white md:text-7xl md:leading-[5rem]" text="Face Detection" />
         <div className="justify-center">
-        <input type="file" onChange={handleFileChange} />
+          <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white" htmlFor="file_input">Upload
+            file</label>
+          <input onChange={handleFileChange}
+              className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+              id="file_input" type="file"/>
+          <p className="mt-1 text-sm text-gray-500 dark:text-gray-300" id="file_input_help">SVG, PNG, JPG or WEBP</p>
+          <Badge variant="outline"
+                 className="bg-gradient-to-r from-amber-400 via-red-500 to-indigo-600 bg-clip-text text-transparent">*Please
+            upload image below 1mb</Badge>
         </div>
         <div className="justify-center">
-        <Button onClick={handleUpload} disabled={!selectedFile || loading} variant="outline">
-          {loading ? 'Processing...' : 'Upload'}
-        </Button>
+          <Button onClick={handleUpload} disabled={!selectedFile || loading} variant="outline">
+            {loading ? 'Processing...' : 'Upload'}
+          </Button>
         </div>
         {detectedImage && (
-        <div style={{ marginTop: '20px' }}>
-          <h2>Detected Faces</h2>
-          <Image src={detectedImage} alt="Detected faces" style={{ maxWidth: '100%' }} />
+            <div style={{marginTop: '20px'}}>
+              <h2>Detected Faces</h2>
+              <Image src={detectedImage} alt="Detected faces" style={{ maxWidth: '100%' }} />
         </div>
       )}
       </div>
