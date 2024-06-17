63b36469ba3dea732fe3217da255ddfb5a10a90a
Parth Sinha <sinhaparth555@gmail.com>
2024-06-17 21:18:10 +0100

    changed the api url

--- face-detection-fe/src/app/face-detection/page.tsx
+++ face-detection-fe/src/app/face-detection/page.tsx
@@ -34,7 +34,7 @@ export default function Home() {
         formData.append('file', selectedFile);
 
         try {
-            const response = await axios.post("https://ai.pksinha.co.uk/api/v1/face-detection", formData, {
+            const response = await axios.post("https://api.pksinha.co.uk/api/v1/face-detection", formData, {
                 responseType: 'blob',
                 headers: {
                     'Content-Type': 'multipart/form-data'
